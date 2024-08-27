66c24ef44392dc7148c0ca3d1a83679820aef106
Tania Fatma Wati <tania.fatmawati20@gmail.com>
2024-08-28 06:58:18 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,133 +12,82 @@ int main(){
     char cipherkey[20];
     char roundKeys[11][4][4][2];
 
-    // Memasukkan plaintext
-    cout<<"Masukkan plaintext (maksimum 16 karakter): ";cin.getline(plaintext,20);
+    // Input plaintext
+    cout << "Enter plaintext (maximum 16 characters): "; 
+    cin.getline(plaintext, 20);
 
     if (strlen(plaintext) > 16) {
-   	    cout<<"Plaintext terlalu panjang. Maksimum 16 karakter."<<endl;
+        cout << "Plaintext is too long. Maximum 16 characters." << endl;
     } else {
-   	    // Memasukkan cipherkey
-   	    cout<<"Masukkan cipherkey (maksimum 16 karakter): ";cin.getline(cipherkey,20);
+        // Input cipherkey
+        cout << "Enter cipherkey (maximum 16 characters): "; 
+        cin.getline(cipherkey, 20);
 
-   	    if (strlen(cipherkey) > 16) {
-   		    cout<<"Cipherkey terlalu panjang. Maksimum 16 karakter."<<endl;
-   	    } else {
-		    // Memastikan plaintext dan cipherkey memiliki panjang yang sama
-   		    if (strlen(plaintext) != strlen(cipherkey)) {
-   			    cout<<"Panjang plaintext dan cipherkey harus sama."<<endl;
-   		    } else {
-			    // Mengubah plaintext ke format desimal
-   			    int despt[4][4];
-			    char resultEncryption[4][4][2];
-   			    stringToMatrix(plaintext, despt);
-   			    cout<<"\n Matrix of Byte Plaintext:"<<endl;
-   			    for (int row = 0; row < 4; row++) {
-				    cout<<" ";
-   				    for (int col = 0; col < 4; col++) {
-					    cout<<despt[row][col]<<"\t";
-				    }
-				    cout<<endl;
-			    }
+        if (strlen(cipherkey) > 16) {
+            cout << "Cipherkey is too long. Maximum 16 characters." << endl;
+        } else {
+            // Ensure plaintext and cipherkey have the same length
+            if (strlen(plaintext) != strlen(cipherkey)) {
+                cout << "Plaintext and cipherkey must have the same length." << endl;
+            } else {
+                // Convert plaintext to decimal format
+                int despt[4][4];
+                char resultEncryption[4][4][2];
+                stringToMatrix(plaintext, despt);
 
-   			    // Mengubah desimal plaintext format heksadesimal 16-bit
-   			    char state[4][4][2];
-   			    matrixToHex(despt, state);
-   			    cout<<"\n State (Plaintext):"<<endl;
-   			    for (int row = 0; row < 4; row++) {
-				    cout<<" ";
-   				    for (int col = 0; col < 4; col++) {
-					    for (int i = 0; i < 2; i++){
-						    cout<<state[row][col][i];
-					    }
-					    cout<<"\t";
-				    }
-				    cout<<endl;
-   			    }
-			    
-			    // Mengubah cipherkey ke format desimal
-   			    int desck[4][4];
-   			    stringToMatrix(cipherkey, desck);
-   			    cout<<"\n Matrix of Byte Cipherkey:"<<endl;
-   			    for (int row = 0; row < 4; row++) {
-				    cout<<" ";
-   				    for (int col = 0; col < 4; col++) {
-					    cout<<desck[row][col]<<"\t";
-				    }
-				    cout<<endl;
-   			    }
+                // Convert decimal plaintext to hexadecimal 16-bit format
+                char state[4][4][2];
+                matrixToHex(despt, state);
 
-   			    // Mengubah desimal cipherkey format heksadesimal 16-bit
-   			    char key[4][4][2];
-   			    matrixToHex(desck, key);
-   			    cout<<"\n Cipherkey (hexadecimal):"<<endl;
-   			    for (int row = 0; row < 4; row++) {
-				    cout<<" ";
-   				    for (int col = 0; col < 4; col++) {
-					    for (int i = 0; i < 2; i++){
-						    cout<<key[row][col][i];
-					    }
-					    cout<<"\t";
-				    }
-				    cout<<endl;
-   			    }
+                // Convert cipherkey to decimal format
+                int desck[4][4];
+                stringToMatrix(cipherkey, desck);
 
-                cout<<"\n\n\n\n";
-                cout<<" ======================================== "<<endl;
-                cout<<" ---------- Pembangkitan Kunci ---------- "<<endl;
-                cout<<" ======================================== "<<endl;
+                // Convert decimal cipherkey to hexadecimal 16-bit format
+                char key[4][4][2];
+                matrixToHex(desck, key);
 
-                // Kunci ronde 0 adalah cipherkey
-                cout<<"\n ---------- \n ROUND 0 : \n ---------- "<<endl;
-    			for (int row = 0; row < 4; row++) {
-        			for (int col = 0; col < 4; col++) {
-               	        for (int i = 0; i < 2; i++){
+                // Round key 0 is the cipherkey
+                for (int row = 0; row < 4; row++) {
+                    for (int col = 0; col < 4; col++) {
+                        for (int i = 0; i < 2; i++){
                             roundKeys[0][row][col][i] = key[row][col][i];
                         }
-        			}
-    			}
-                tampilmatrix (roundKeys[0]);
+                    }
+                }
 
-    			// Membuat round keys
-    			for (int round = 1; round <= 10; round++) {
-                    cout<<"\n ---------- \n ROUND "<<round<<" : \n ---------- "<<endl;
-        			char temp[4][2]; // Penyimpanan kolom sementara
+                // Generate round keys
+                for (int round = 1; round <= 10; round++) {
+                    char temp[4][2]; // Temporary column storage
 
-        			// RotWord (Shift Byte ke atas)
-        			for (int i = 0; i < 4; i++) {
-            		    temp[i][0] = roundKeys[round - 1][i][3][0];
-            		    temp[i][1] = roundKeys[round - 1][i][3][1];
-        			}
-        			rotWord(temp);
+                    // RotWord (Shift Byte upwards)
+                    for (int i = 0; i < 4; i++) {
+                        temp[i][0] = roundKeys[round - 1][i][3][0];
+                        temp[i][1] = roundKeys[round - 1][i][3][1];
+                    }
+                    rotWord(temp);
 
-        			// SubBytes
-        			subBytes(temp);
+                    // SubBytes
+                    subBytes(temp);
 
-                    // Kolom 1 ronde ke-i
+                    // First column of round i
                     for (int i = 0; i < 4; i++){
-                        xorfisrtcolumn (round, temp[i], roundKeys[round - 1][i][0], Rcon[i], roundKeys[round][i][0]);
+                        xorfisrtcolumn(round, temp[i], roundKeys[round - 1][i][0], Rcon[i], roundKeys[round][i][0]);
                     }
 
-        			// Kolom 2-4 ronde ke-i
-        			for (int col = 1; col < 4; col++) {
-            		    for (int row = 0; row < 4; row++) {
-                            xornextcolumn (roundKeys[round][row][col - 1], roundKeys[round - 1][row][col], roundKeys[round][row][col]);
+                    // Columns 2-4 of round i
+                    for (int col = 1; col < 4; col++) {
+                        for (int row = 0; row < 4; row++) {
+                            xornextcolumn(roundKeys[round][row][col - 1], roundKeys[round - 1][row][col], roundKeys[round][row][col]);
                         }
-        			}
-
-                    // Mencetak matriks pada setiap ronde
-                    tampilmatrix (roundKeys[round]);
-    			}
+                    }
+                }
 
                 // AES
-                cout<<"\n\n\n";
-                cout<<" ======================================== "<<endl;
-                cout<<" ------------ Proses Enkripsi ----------- "<<endl;
-                cout<<" ======================================== "<<endl;
                 aesEncrypt(state, roundKeys, resultEncryption);
-                cout<<"\n\n\n";
-                cout<<" ======================================== "<<endl;
-                cout<<" |  Hasil Enkripsi :   ";
+                cout << "\n\n\n";
+                cout << " ======================================== " << endl;
+                cout << " |  Encryption Result :   ";
                 int tampunganakhir[2], blocktampungan[2][4], tampbiner[8];
                 for (int row = 0; row < 4; row++){
                     for (int col = 0; col < 4; col++){
@@ -149,20 +98,20 @@ int main(){
                             }
                         }
                         for (int i = 0; i < 2; i++){
-                            desToBiner(tampunganakhir[i],blocktampungan[i]);
+                            desToBiner(tampunganakhir[i], blocktampungan[i]);
                         }
                         for (int i = 0; i < 4; i++){
                             tampbiner[i] = blocktampungan[0][i];
                             tampbiner[i + 4] = blocktampungan[1][i];
                         }
                         char ascii = static_cast<char>(biner8ToDes(tampbiner));
-                        cout<<ascii;
+                        cout << ascii;
                     }
                 }
-                cout<<"\n ======================================== "<<endl;
-                cout<<endl<<endl<<endl;
+                cout << "\n ======================================== " << endl;
+                cout << endl << endl << endl;
             }
         }
     }
-	return 0;
+    return 0;
 }
